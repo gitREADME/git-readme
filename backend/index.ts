08d0fd213b35d9d19ce6fa1e037e3f2ffc541afa
gitREADME <dummymisrha@gmail.com>
2026-07-05 19:04:02 +0530

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -45,7 +45,7 @@ app.use(
 		resave: false,
 		saveUninitialized: false,
 		cookie: {
-			sameSite : isProduction ? "none" : "lax",
+			sameSite : "lax",
 			httpOnly: true,
 			secure: isProduction,
 			maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
