9b7083152abc6405645cf58898de73dede0abc0a
gitREADME <dummymisrha@gmail.com>
2026-07-05 19:06:40 +0530

    Update isLoggedIn.ts

--- backend/utils/isLoggedIn.ts
+++ backend/utils/isLoggedIn.ts
@@ -4,8 +4,12 @@ import appError from "../error/appError.js";
 export default function isLoggedIn(req : Request , res : Response , next : NextFunction){
 	if(process.env.NODE_ENV === "test") return next(); //for testing purposes
 
-	console.log(req.session);
-	console.log(req.session.githubId);
+console.log({
+  sessionID: req.sessionID,
+  githubId: req.session.githubId,
+  cookieHeader: req.headers.cookie,
+  origin: req.headers.origin,
+});
 	
 	if(req.session && req.session.githubId){
 		next();
